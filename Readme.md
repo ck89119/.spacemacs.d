70594fc453af6ed356d5a6e16b659eb3b3067a41
ck89119 <ck89119@gmail.com>
2016-02-01 21:44:00 +0800

    complete install.sh

--- Readme.md
+++ Readme.md
@@ -1,1 +1,9 @@
 # .spacemacs.d
+
+## install
+
+  curl -o - https://raw.github.com/ck89119/.vim/master/auto-install.sh | sh
+
+or
+
+  wget -O - https://raw.github.com/ck89119/.vim/master/auto-install.sh | sh
--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 $!/bin/bash
 
 cd ~
+
+if [ -e ".spacemacs.d" ] && rm -rf .spacemacs.d
+git clone https://github.com/ck89119/.spacemacs.d.git ~/.spacemacs.d
+
+if [ -e ".emacs.d" ] && rm -rf .emacs.d
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
 ln -s .spacemacs.d/.spacemacs .spacemacs
